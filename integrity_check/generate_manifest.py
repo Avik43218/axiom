a75098bafd60cd6906baa0ea74cdb17b831b680e
Avik43218 <roychoudhuryavik005@gmail.com>
2026-03-10 19:03:55 +0530

    Program for generating project manifest

---
+++ integrity_check/generate_manifest.py
@@ -0,0 +1,48 @@
+import hashlib
+import os
+import json
+import hashlib
+from datetime import datetime
+
+def calculateSHA256(filePath: str) -> str:
+    sha256_hash = hashlib.sha256()
+    with open(filePath, 'rb') as file:
+        for byteBlock in iter(lambda: file.read(4096), b""):
+            sha256_hash.update(byteBlock)
+
+    return sha256_hash.hexdigest()
+
+def generateManifest(sourceDir: str):
+    manifest = {
+        "projectName": "Axiom",
+        "timeGeneratedAt": datetime.utcnow().isoformat() + "Z",
+        "files": []
+    }
+
+    allHashes = ""
+
+    for root, dirs, files in os.walk(sourceDir):
+        for f in sorted(files):
+            if f.endswith(('.py', '.cpp', '.ini')):
+                filePath = os.path.join(root, f)
+                fileHash = calculateSHA256(filePath)
+
+                cleanPath = os.path.relpath(filePath, sourceDir)
+
+                manifest["files"].append({
+                    "path": cleanPath,
+                    "hash": fileHash
+                })
+
+                allHashes += fileHash
+
+    manifest["masterHash"] = hashlib.sha256(allHashes.encode()).hexdigest()
+
+    with open("manifest.json", "w") as manifestFile:
+        json.dump(manifest, manifestFile, indent=4)
+
+    print("Manifest created successfully!")
+    print(f"Master hash: {manifest['masterHash']}")
+
+if __name__ == "__main__":
+    generateManifest("../src")
